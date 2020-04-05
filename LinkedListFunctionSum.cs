9f358d5fa57df1b812c80c5f0da2648f10642b18
ilyich1 <46596316+ilyich1@users.noreply.github.com>
2020-04-05 23:15:11 +0300

    Add files via upload

---
+++ LinkedListFunctionSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsDataStructures
+{
+    public class LinkedListFunctionSum
+    {
+        public List<int> LinkedListSum(LinkedList list1, LinkedList list2)
+        {
+            int length1 = list1.Count();
+            int length2 = list2.Count();
+
+            Node node1 = list1.head;
+            Node node2 = list2.head;
+
+            List<int> list3 = new List<int>();
+
+            if (length1 == length2)
+            {
+                for (int i=0;i<length1;i++)
+                {
+                    list3.Add(node1.value + node2.value);
+                    node1 = node1.next;
+                    node2 = node2.next;
+                }
+            }
+            return list3;
+        }
+    }
+}
+
