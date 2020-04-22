c18f65fe231dcd2505542825a004851f3d796b1a
ilyich1 <46596316+ilyich1@users.noreply.github.com>
2020-04-22 17:11:36 +0300

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Queue<T>
+    {
+        List<T> queue;
+        int count;
+
+        public Queue()
+        {
+            // инициализация внутреннего хранилища очереди
+            queue = new List<T>();
+            count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            // вставка в хвост
+            queue.Add(item);
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            // выдача из головы
+            if (count!=0)
+            {
+                T cache = queue[0];
+                queue.RemoveAt(0);
+                count--;
+                return cache;
+            }
+            return default(T); // если очередь пустая
+        }
+
+        public int Size()
+        {
+            if (count !=0)
+            {
+                return count;
+            }
+            return 0; // размер очереди
+        }
+
+    }
+}
