d8e4d97cfdc8a08f2dd035589072b3fa36eec0bd
Aleksandr <al.novikov08@gmail.com>
2020-11-04 23:49:52 +0300

    DepthFirstSearch - new method

--- SimpleGraph.cs
+++ SimpleGraph.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures2
 {
-    public class Vertex
+    public class Vertex<T>
     {
-        public int Value;
-        public Vertex(int val)
+        public bool Hit;
+        public T Value;
+        public Vertex(T val)
         {
             Value = val;
+            Hit = false;
         }
     }
 
-    public class SimpleGraph
+    public class SimpleGraph<T>
     {
-        public Vertex[] vertex;
+        public Vertex<T>[] vertex;
         public int[,] m_adjacency;
         public int max_vertex;
 
@@ -24,12 +26,12 @@ namespace AlgorithmsDataStructures2
         {
             max_vertex = size;
             m_adjacency = new int[size, size];
-            vertex = new Vertex[size];
+            vertex = new Vertex<T>[size];
 
             capacity = 0;
         }
 
-        public void AddVertex(int value)
+        public void AddVertex(T value)
         {
             // ваш код добавления новой вершины 
             // с значением value 
@@ -37,7 +39,7 @@ namespace AlgorithmsDataStructures2
 
             if (capacity < max_vertex)
             {
-                Vertex newVertex = new Vertex(value);
+                Vertex<T> newVertex = new Vertex<T>(value);
                 vertex[capacity] = newVertex;
                 capacity++;
             }
@@ -52,7 +54,7 @@ namespace AlgorithmsDataStructures2
             if (capacity > 0 && (vertex[v] != null))
             {
                 vertex[v] = null;
-                
+
                 for (int i = 0; i < capacity; i++)
                 {
                     m_adjacency[v, i] = 0;
@@ -96,6 +98,77 @@ namespace AlgorithmsDataStructures2
                 m_adjacency[v1, v2] = 0;
                 m_adjacency[v2, v1] = 0;
             }
+        }
+
+        public List<Vertex<T>> DepthFirstSearch(int VFrom, int VTo)
+        {
+            // Узлы задаются позициями в списке vertex.
+            // Возвращается список узлов -- путь из VFrom в VTo.
+            // Список пустой, если пути нету.
+
+            Stack<Vertex<T>> Result = new Stack<Vertex<T>>();
+            List<Vertex<T>> Path = new List<Vertex<T>>();
+
+            if (VFrom >= capacity || VTo >= capacity || VFrom < 0 || VTo < 0) // Обработка некорректного ввода
+                return Path;
+            if (VFrom == VTo) { Path.Add(vertex[VFrom]); return Path; }
+
+            foreach (Vertex<T> item in vertex)
+            {
+                if (item != null)
+                    item.Hit = false;
+            }
+
+            Vertex<T> Select = vertex[VFrom];
+
+            bool newSelect = true;
+            int newSelectIndex = -1;
+
+            while (Select != null)
+            {
+                if (newSelect == true)
+                {
+                    Result.Push(Select);
+                    Select.Hit = true;
+                    newSelect = false;
+                }
+
+                for (int i = 0; i < capacity; i++)
+                {
+                    if (IsEdge(Array.IndexOf(vertex, Select), i) == true)
+                    {
+                        if (vertex[i] == vertex[VTo])
+                        {
+                            Result.Push(vertex[i]);
+                            Path.AddRange(Result);
+                            Path.Reverse();
+
+                            return Path;
+                        }
+
+                        if (newSelect == false && vertex[i].Hit == false) // Код для запоминания смежного узла, у которого не было посещения
+                        {
+                            newSelectIndex = i;
+                            newSelect = true;
+                        }
+                    }
+                }
+
+                if (newSelectIndex != -1) { Select = vertex[newSelectIndex]; }
+
+                if (newSelect == false) // Если нет непосещенных узлов
+                {
+                    Result.Pop();
+                    if (Result.Count > 0)
+                    {
+                        Select = Result.Peek();
+                        newSelect = false;
+                    }
+                    else
+                        Select = null;
+                }
+            }
+            return Path;
         }
     }
 }
